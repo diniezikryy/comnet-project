e90c2c6e921f89caf76a5dca5fc39adf20f30183
Dinie Zikry <33160937+DinieZikry@users.noreply.github.com>
2024-07-12 15:23:36 +0800

    changed password hashing

--- models.py
+++ models.py
@@ -1,6 +1,14 @@
+import os
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+
+from cryptography.exceptions import InvalidKey
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+
 from extensions import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,10 +17,37 @@ class User(db.Model, UserMixin):
     nfc_tag = db.Column(db.String(150), unique=True, nullable=True)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        salt = os.urandom(16)
+        kdf = Scrypt(
+            salt=salt,
+            length=32,
+            n=2 ** 14,
+            r=8,
+            p=1,
+            backend=default_backend()
+        )
+        key = kdf.derive(password.encode('utf-8'))
+        self.password_hash = f'{urlsafe_b64encode(salt).decode("utf-8")}:{urlsafe_b64encode(key).decode("utf-8")}'
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        try:
+            salt, key = self.password_hash.split(':')
+            salt = urlsafe_b64decode(salt.encode('utf-8'))
+            key = urlsafe_b64decode(key.encode('utf-8'))
+            kdf = Scrypt(
+                salt=salt,
+                length=32,
+                n=2 ** 14,
+                r=8,
+                p=1,
+                backend=default_backend()
+            )
+            kdf.verify(password.encode('utf-8'), key)
+            return True
+        except Exception as e:
+            print(f"Password verification error: {e}")
+            return False
+
 
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
