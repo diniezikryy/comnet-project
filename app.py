023f3948f51ab7dce982ee2fbb5c3ee535e2821d
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2024-07-13 19:55:25 +0800

    fixed registration & login issue, every user has a name now

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def logs():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data)
+        user = User(name=form.name.data, username=form.username.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
